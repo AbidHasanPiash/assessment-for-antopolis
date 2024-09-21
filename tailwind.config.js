d9e0768864f9e7fa0cb43d60fb1ab40742d86969
AႦιԃ Hαʂαɳ <meghpiash@gmail.com>
2024-09-21 10:10:50 +0600

    ADDED: food and client images

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'primary': "#880808",
+        'primary-light': "#a52a2a",
+
+        'variant-1': "#0a4669",
+        'variant-1-light': "#0a3659",
+        'primary-light': "#a52a2a",
+
+        'variant-2': "#953553",
+        'variant-2-light': "#a95c68",
+
+        'variant-3': "#006666",
+        'variant-3-light': "#003333",
       },
     },
   },
